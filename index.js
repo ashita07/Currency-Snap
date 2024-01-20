70019f5f3be55532e54e886314cdc96f13b2ee42
Ashita <ashitam2407@gmail.com>
2024-01-20 09:20:24 +0530

    adding countrylist access

---
+++ index.js
@@ -0,0 +1,7 @@
+const base_url= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+
+const dropdowns=document.querySelector(".dropdown select");
+
+for(code in countryList){
+    console.log(code);
+}
