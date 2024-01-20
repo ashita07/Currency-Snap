82c0410f1c833f0e1774f1cc2b03e5c1cc8c63ea
Ashita <ashitam2407@gmail.com>
2024-01-20 13:39:49 +0530

    added dynamically changing dropdowns with flag image

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const base_url= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
 
-const dropdowns=document.querySelector(".dropdown select");
+const button = document.querySelector("form button");
 
-for(code in countryList){
-    console.log(code);
+const dropdowns=document.querySelectorAll(".dropdown select");
+
+for(let select of dropdowns){
+
+for (code in countryList) {
+  let newOption = document.createElement("option")
+  newOption.innerText=code
+  newOption.value = code;
+
+if(select.name === "from" && code ==="USD"){
+    newOption.selected="selected";}
+    if(select.name === "to" && code ==="INR"){
+    newOption.selected="selected";   
 }
+  select.append(newOption);
+}
+select.addEventListener("change",(evt)=>{
+updateFlag(evt.target);
+})
+}
+
+
+const updateFlag=(element)=>{
+  let currCode =element.value;
+  let countryCode = countryList[currCode];
+  let newScr = `https://flagsapi.com/${countryCode}/flat/64.png`;
+ const img= element.parentElement.querySelector("img");
+ img.src=newScr;
+}
+
+button.addEventListener("click",(event)=>{
+event.preventDefault();
+let amt = document.querySelector(" input")
+let value=amt.value;
+if(value===""){
+    alert("please enter anount to be converted")
+}
+if(value<0){
+    alert("invalid input")
+}
+})
